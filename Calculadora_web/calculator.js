d1e900b2d77463f2e7de8752675671ebdaf5983f
alextec2022 <113274020+alextec2022@users.noreply.github.com>
2023-12-14 20:29:41 -0600

    first commit, calculadora

---
+++ Calculadora_web/calculator.js
@@ -0,0 +1,25 @@
+// calculator.js
+function add(a, b) {
+    return a + b;
+  }
+  
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  function divide(a, b) {
+    if (b !== 0) {
+      return a / b;
+    } else {
+      return "Error: Division de cero";
+    }
+  }
+  
+  console.log("suma:", add(5, 3));
+  console.log("resta:", subtract(5, 3));
+  console.log("Multiplicacion:", multiply(5, 3));
+  console.log("Division:", divide(5, 3));
