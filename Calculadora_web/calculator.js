cec7651a30cdbb7a69926d4976ecea0a614c6c5b
alextec2022 <113274020+alextec2022@users.noreply.github.com>
2023-12-14 20:54:30 -0600

    Update calculator.js

--- Calculadora_web/calculator.js
+++ Calculadora_web/calculator.js
@@ -1,62 +1,27 @@
 // calculator.js
-const readline = require('readline-sync');
-
-function sumar(a, b) {
+function add(a, b) {
   return a + b;
 }
 
-function restar(a, b) {
+function subtract(a, b) {
   return a - b;
 }
 
-function multiplicar(a, b) {
+function multiply(a, b) {
   return a * b;
 }
 
-function dividir(a, b) {
+function divide(a, b) {
   if (b !== 0) {
     return a / b;
   } else {
-    return "Error: División por cero";
+    return "Error: Division ";
   }
 }
 
-function obtenerNumero(mensaje) {
-  return parseFloat(readline.question(mensaje));
-}
+console.log("suma:", add(5, 3));
+console.log("resta:", subtract(5, 3));
+console.log("multiplicacion:", multiply(5, 3));
+console.log("Division:", divide(5, 3));
 
-console.log("Calculadora Simple en JavaScript");
 
-const numero1 = obtenerNumero("Ingrese el primer número: ");
-const numero2 = obtenerNumero("Ingrese el segundo número: ");
-
-console.log(`\nOperaciones:`);
-console.log(`1. Sumar`);
-console.log(`2. Restar`);
-console.log(`3. Multiplicar`);
-console.log(`4. Dividir`);
-
-const opcion = readline.questionInt("\nSeleccione la operación (1-4): ");
-
-let resultado;
-
-switch (opcion) {
-  case 1:
-    resultado = sumar(numero1, numero2);
-    console.log(`\nResultado: ${numero1} + ${numero2} = ${resultado}`);
-    break;
-  case 2:
-    resultado = restar(numero1, numero2);
-    console.log(`\nResultado: ${numero1} - ${numero2} = ${resultado}`);
-    break;
-  case 3:
-    resultado = multiplicar(numero1, numero2);
-    console.log(`\nResultado: ${numero1} * ${numero2} = ${resultado}`);
-    break;
-  case 4:
-    resultado = dividir(numero1, numero2);
-    console.log(`\nResultado: ${numero1} / ${numero2} = ${resultado}`);
-    break;
-  default:
-    console.log("Opción no válida");
-}
